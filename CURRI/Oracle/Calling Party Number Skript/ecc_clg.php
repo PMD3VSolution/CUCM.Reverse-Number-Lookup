d208ef1474b90e4603867a16af286cf1318755cd
PMD3VSolution <petermuchau@aol.com>
2018-08-04 14:29:57 +0200

    oracle

---
+++ CURRI/Oracle/Calling Party Number Skript/ecc_clg.php
@@ -0,0 +1,12 @@
+<?php
+#
+#################################################
+#   Dateiname:               ecc.php
+#    Dateiversion:           1.0
+#    Ersteller:              Peter Muchau
+#    Beschreibung:           ecc.php
+#    Letzter Bearbeiter:     Peter Muchau
+#    Datum der Bearbeitung:  15.05.2018
+#################################################
+#
+?>
